1dde4f92f3580477c72f427607c389df343c4f13
Mihir Lohakare <117346340+Lohakaremihir14@users.noreply.github.com>
2025-01-10 21:07:03 +0530

    Arithematic operation on program

--- master/FS-Gitub/src/com/pack1/ClassA.java
+++ master/FS-Gitub/src/com/pack1/ClassA.java
@@ -8,9 +8,17 @@ public class ClassA
 		int y=20;
 		
 		int z=x+y;
+		int i=x-y;
+		int j=x/y;
+		int k=x*y;
 		
-		System.out.println(z);
+		System.out.println("Addition : "+z);
+		System.out.println("Substraction : "+i);
+		System.out.println("Division : "+j);
+		System.out.println("Multiplication : "+k);
+		
 	}
+
 	public static void main(String[] args) 
 	{
 		new ClassA().meth1();
